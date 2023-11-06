ec3b9f23ca7d72e68c7d2cd5bfb098da8d5b4c68
John Dykes <98johndykes@gmail.com>
2023-11-05 20:48:59 -0500

    add product model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_home)
+    path('', views.hello_world),
+    path('echo', views.echo)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
+from typing import Any, Dict
+import json
 
-def api_home(request, *args, **kwargs):
-    data = {"message": "This is the message!"}
+def hello_world(request: HttpRequest, *args: any, **kwargs: any) -> JsonResponse:
+    data: Dict[str, str] = {"message": "This is the message!"}
     return JsonResponse(data=data)
+
+def echo(request: HttpRequest, *args: any, **kwargs: any) -> JsonResponse:
+    body = request.body
+    data: Dict[str, str] = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
